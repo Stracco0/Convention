8901749bf3d828032f7ac3bd727434e4a5fdcdd3
Stracco0 <mirkodimarco2018@gmail.com>
2024-05-22 18:34:31 +0200

    cdn a locale
    ultima modifica prima della consegna

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Convention
+# Convention Dimarco
+
--- Sito_convention/dependences.php
+++ Sito_convention/dependences.php
@@ -1,5 +1,5 @@
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<script src="./dependences/jquery.js"></script>
+<link rel="stylesheet" href="./dependences/fontawesome-free-5.5.0-web/css/all.css">
+<link rel="stylesheet" href="./dependences/bootstrap-4/css/bootstrap.min.css">
+<link rel="stylesheet" href="./dependences/bootstrap-5.0.2-dist/css/bootstrap.min.css">
+<script src="./dependences/bootstrap-4/js/bootstrap.min.js"></script>
