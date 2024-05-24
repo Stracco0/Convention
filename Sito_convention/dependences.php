0345defa4707fa5aefb8240053ae025b2582cbda
Mirko <133965736+Stracco0@users.noreply.github.com>
2024-05-24 10:26:41 +0200

    Update dependences.php
    Modifica per non mostrare warnings

--- Sito_convention/dependences.php
+++ Sito_convention/dependences.php
@@ -3,3 +3,6 @@
 <link rel="stylesheet" href="./dependences/bootstrap-4/css/bootstrap.min.css">
 <link rel="stylesheet" href="./dependences/bootstrap-5.0.2-dist/css/bootstrap.min.css">
 <script src="./dependences/bootstrap-4/js/bootstrap.min.js"></script>
+<?php
+	error_reporting(0);
+?>
